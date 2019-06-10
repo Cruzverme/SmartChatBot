9ad5fb6b1fdd98749b84767db2052e69887cd828
Cruzverme <grossmino@hotmail.com>
2019-06-09 23:12:58 -0300

    created DB and Migrates

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
 require 'json'
 require 'sinatra'
 require 'sinatra/activerecord'
-  
+
+# Load Models
+Dir["./app/models/*.rb"].each {|file| require file }
+
 require './config/database'
   
 class App < Sinatra::Base
